290fdba96402b8de1275cdaa7a43c81a97a6f16b
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2022-12-10 22:40:19 +0530

    #include<stdio.h> int cars(int n); int main() {     int n;     scanf("%d",&n);     int x=cars(n); } int cars(int n) {     if(n%4==0)     {         printf("%d",n/4);     }     else     {         printf("%d",(n/4)+1);     }     return 0; }

--- Cars.c
+++ Cars.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int cars(int n);
 int main()
 {
     int n;
     scanf("%d",&n);
+    int x=cars(n);
+}
+int cars(int n)
+{
     if(n%4==0)
     {
         printf("%d",n/4);
@@ -11,4 +16,5 @@ int main()
     {
         printf("%d",(n/4)+1);
     }
+    return 0;
 }
