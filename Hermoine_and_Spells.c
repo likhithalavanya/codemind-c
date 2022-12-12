78f3a4c2818d0253362579135264e9dffa855450
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2022-12-12 10:08:07 +0530

    #include<stdio.h> void spells(int a,int b,int c); int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     spells(a,b,c); } void spells(int a,int b,int c) {     if(a>c && b>c)     {         printf("%d",a+b);     }     else if(b>a && c>a)     {         printf("%d",b+c);     }     else if(c>b && a>b)     {         printf("%d",c+a);     } }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int spells(int a,int b,int c);
+void spells(int a,int b,int c);
 int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    int x=spells(a,b,c);
+    spells(a,b,c);
 }
-int spells(int a,int b,int c)
+void spells(int a,int b,int c)
 {
     if(a>c && b>c)
     {
@@ -19,6 +19,5 @@ int spells(int a,int b,int c)
     else if(c>b && a>b)
     {
         printf("%d",c+a);
-    }
-    return 0;
+    }    
 }
