ddd7c5fa909758e6799bd6f0cde30972d309d519
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2022-12-27 20:38:16 +0530

    #include<stdio.h> int main() {     int n,k,m;     scanf("%d%d%d",&n,&k,&m);     int cpb=k*m;     if (n%cpb==0)     {         printf("%d",n/cpb);     }     else     {         printf("%d",n/cpb+1);     } }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
-void bac(int n,int k,int m);
 int main()
 {
     int n,k,m;
     scanf("%d%d%d",&n,&k,&m);
-    bac(n,k,m);
-}
-void bac(int n,int k,int m)
-{
     int cpb=k*m;
     if (n%cpb==0)
     {
