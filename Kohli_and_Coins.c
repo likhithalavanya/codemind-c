a014036e8565a00cc5074e82e9d780cd66a4f0ca
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2022-12-31 20:38:26 +0530

    #include<stdio.h> int kohli(int n); int main() {     int n;     scanf("%d",&n);     int x=kohli(n); } int kohli(int n) {     if (n%5!=0)     {         printf("%d",-1);     }     else     {         printf("%d",n/10 + (n%10)/5);     }     return 0; }

--- Kohli_and_Coins.c
+++ Kohli_and_Coins.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int kohli(int n);
 int main()
 {
     int n;
     scanf("%d",&n);
+    int x=kohli(n);
+}
+int kohli(int n)
+{
     if (n%5!=0)
     {
         printf("%d",-1);
@@ -11,4 +16,5 @@ int main()
     {
         printf("%d",n/10 + (n%10)/5);
     }
+    return 0;
 }
