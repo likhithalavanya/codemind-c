e26803ee5729d0cf96b9d5d026a18d19b51e60b8
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-01-03 23:30:40 +0530

    #include<stdio.h> float circle(int r); int main() {     int r;     scanf("%d",&r);     float x=circle(r); } float circle(int r) {     float a,p;     a=3.14*r*r;     p=2*3.14*r;     printf("%.2f %.2f",a,p);     return 0; }

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-void circle(int r);
+float circle(int r);
 int main()
 {
     int r;
     scanf("%d",&r);
-    circle(r);
+    float x=circle(r);
 }
-void circle(int r)
+float circle(int r)
 {
     float a,p;
     a=3.14*r*r;
     p=2*3.14*r;
     printf("%.2f
 %.2f",a,p);
+    return 0;
 }
