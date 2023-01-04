20238b2533253f7bc063af53358e0a0ca7abfe36
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-01-04 23:10:46 +0530

    #include<stdio.h> #include<math.h> int main() {     int num,num1,c=0,sum=0,rem;     scanf("%d",&num);     num1=num;     while(num1!=0)     {         num1/=10;         c++;     }     num1=num;     while(num1!=0)     {         rem=num1%10;         sum+=pow(rem,c);         num1/=10;         c--;     }     if(sum==num)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num,num1,c=0,sum=0,rem;
+    scanf("%d",&num);
+    num1=num;
+    while(num1!=0)
+    {
+        num1/=10;
+        c++;
+    }
+    num1=num;
+    while(num1!=0)
+    {
+        rem=num1%10;
+        sum+=pow(rem,c);
+        num1/=10;
+        c--;
+    }
+    if(sum==num)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }    
+}
