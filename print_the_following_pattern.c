6ef27675ed411342c71b6aab52f4581d18c4476b
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-01-10 12:18:24 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n-2;j++)         {             printf("%d",j);         }         for(j=n-3;j>=1;j--)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,7 +5,11 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n-2;j++)
+        {
+            printf("%d",j);
+        }
+        for(j=n-3;j>=1;j--)
         {
             printf("%d",j);
         }
