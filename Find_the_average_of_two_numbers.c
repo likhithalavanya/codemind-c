0dd3c1d937affe6fa0e948b0e3c691f6bc9c97f9
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-05-02 12:12:46 +0530

    #include<stdio.h> void ave(int a,int b); int main() {     int a,b;     scanf("%d%d",&a,&b);     ave(a,b); } void ave(int a,int b) {     float c;     c=(a+b)/2.0;     printf("%.4f",c); }

--- Find_the_average_of_two_numbers.c
+++ Find_the_average_of_two_numbers.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int ave(int a,int b);
+void ave(int a,int b);
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    float x=ave(a,b);
+    ave(a,b);
 }
-int ave(int a,int b)
+void ave(int a,int b)
 {
     float c;
     c=(a+b)/2.0;
     printf("%.4f",c);
-    return c;
 }
