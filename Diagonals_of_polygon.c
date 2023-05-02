e9104afdf54add5353d8b0fe68dfc6812761f2a4
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-05-02 10:47:21 +0530

    #include<stdio.h> void poly(int n); int main() {     int n;     scanf("%d",&n);     poly(n); } void poly(int n) {     int a;     a=(n*(n-3)*0.5);     printf("%d",a); }

--- Diagonals_of_polygon.c
+++ Diagonals_of_polygon.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int poly(int n);
+void poly(int n);
 int main()
 {
     int n;
     scanf("%d",&n);
-    int x=poly(n);
+    poly(n);
 }
-int poly(int n)
+void poly(int n)
 {
     int a;
     a=(n*(n-3)*0.5);
     printf("%d",a);
-    return a;
 }
