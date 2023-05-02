9b2cd3d6a220483f4f99a0aa78e283b3ea6f74fc
likhithalavanya <119840461+likhithalavanya@users.noreply.github.com>
2023-05-02 12:17:29 +0530

    #include<stdio.h> float mul(float a,float b); int main() {     float a,b;     scanf("%f%f",&a,&b);     float x=mul(a,b); } float mul(float a,float b) {     float c;     c=a*b;     printf("%.2f",c);     return c; }

--- Program_to_Multiply_Two_Floating-Point_Numbers.c
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+float mul(float a,float b);
 int main()
 {
-    float a,b,c;
+    float a,b;
     scanf("%f%f",&a,&b);
+    float x=mul(a,b);
+}
+float mul(float a,float b)
+{
+    float c;
     c=a*b;
     printf("%.2f",c);
+    return c;
 }
